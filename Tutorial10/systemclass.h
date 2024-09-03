a6248bae9ad20fd48262adbec5c300c2f3709a9c
shan <115542178+shan1177@users.noreply.github.com>
2024-09-03 23:06:49 +0530

    Specular Lighting

---
+++ Tutorial10/systemclass.h
@@ -0,0 +1,48 @@
+#ifndef _SYSTEMCLASS_H_
+#define _SYSTEMCLASS_H_
+
+// Pre-processing Directives
+#define WIN32_LEAN_AND_MEAN
+
+// Includes
+#include <windows.h>
+
+// Class Includes
+#include "inputclass.h"
+#include "applicationclass.h"
+
+// Class name: SystemClass
+class SystemClass
+{
+public:
+	SystemClass();
+	SystemClass(const SystemClass&);
+	~SystemClass();
+	
+	bool Initialize();
+	void Shutdown();
+	void Run();
+
+	LRESULT CALLBACK MessageHandler(HWND, UINT, WPARAM, LPARAM);
+
+private:
+	bool Frame();
+	void InitializeWindows(int&, int&);
+	void ShutdownWindows();
+
+private:
+	LPCWSTR m_applicationName;
+	HINSTANCE m_hinstance;
+	HWND m_hwnd;
+
+	InputClass* m_Input;
+	ApplicationClass* m_Application;
+};
+
+// Function Prototypes
+static LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Globals
+static SystemClass* ApplicationHandle = 0;
+
+#endif
